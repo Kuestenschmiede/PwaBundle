ebba6893779433cc493f85ec181bf50bd97a2093
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2019-08-22 10:28:20 +0200

    add missing translations

--- Resources/contao/languages/en/tl_calendar_events.php
+++ Resources/contao/languages/en/tl_calendar_events.php
@@ -15,3 +15,7 @@
 $GLOBALS['TL_LANG']['tl_calendar_events']['pushOnPublish'] = ['Send push notification on publish', 'When set, a push notification will be sent for this event.'];
 $GLOBALS['TL_LANG']['tl_calendar_events']['subscriptionTypes'] = ['Subscription types', 'Choose the subscription types that shall be notified.'];
 $GLOBALS['TL_LANG']['tl_calendar_events']['pnSendDate'] = ["Send date", "Choose a day on which the push notification should be sent."];
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['sendDoublePn'] = ["Sending a push message on a specific date", "Set this checkbox if you want to send a push message for publication and at a specified date."];
+$GLOBALS['TL_LANG']['tl_calendar_events']['sendPn'] = ["PN send", "Send a push message for this event."];
+$GLOBALS['TL_LANG']['tl_calendar_events']['resetSentFlag'] = ["Reset transmission status", "Resets the status in the database so that an automated push message is sent again for this event."];
