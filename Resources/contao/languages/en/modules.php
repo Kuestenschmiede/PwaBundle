22ea6186316c0cb2941fde956ea1f21c727187b2
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2020-01-14 17:55:35 +0100

    changed be module typo

--- Resources/contao/languages/en/modules.php
+++ Resources/contao/languages/en/modules.php
@@ -14,7 +14,7 @@
 $GLOBALS['TL_LANG']['FMD']['pwa'] = array('PWA: add manifest', 'Adds the configured pwa functionality to the application.');
 $GLOBALS['TL_LANG']['FMD']['push-subscription']	= array('PWA: push-subscription', 'Provides a mechanism to subscribe to push notifications.');
 
-$GLOBALS['TL_LANG']['MOD']['c4g_pwa_configuration'] = array('PWA configuration', 'Configuration of pwa functionality.');
-$GLOBALS['TL_LANG']['MOD']['c4g_webpush_configuration'] = array('Web push configuration', 'Configuration of the web push service.');
-$GLOBALS['TL_LANG']['MOD']['c4g_push_notification'] = array('Send test notification', 'Send a test push notification. This function is not intended for productive use.');
-$GLOBALS['TL_LANG']['MOD']['c4g_push_subscription_type'] = array('Push subscription types', 'Define different types of push subscriptions.');
+$GLOBALS['TL_LANG']['MOD']['c4g_pwa_configuration'] = array('PWA: configuration', 'Configuration of pwa functionality.');
+$GLOBALS['TL_LANG']['MOD']['c4g_webpush_configuration'] = array('PWA: Web push configuration', 'Configuration of the web push service.');
+$GLOBALS['TL_LANG']['MOD']['c4g_push_notification'] = array('PWA: Send test notification', 'Send a test push notification. This function is not intended for productive use.');
+$GLOBALS['TL_LANG']['MOD']['c4g_push_subscription_type'] = array('PWA: Push subscription types', 'Define different types of push subscriptions.');
