55f62262e9f083f78efec2572a4139df15b3327b
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2021-03-11 12:33:12 +0100

    Modifications for con4gis 7.3

--- Classes/Callbacks/PageCallback.php
+++ Classes/Callbacks/PageCallback.php
@@ -10,7 +10,6 @@
  * @copyright  Küstenschmiede GmbH Software & Design
  * @link       https://www.con4gis.org
  */
-
 namespace con4gis\PwaBundle\Classes\Callbacks;
 
 use con4gis\PwaBundle\Entity\PwaConfiguration;
