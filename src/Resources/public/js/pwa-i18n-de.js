748df52200c3142f8f1fc018e5396601a52a4e8e
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-01-22 11:42:51 +0100

    Version preps

--- src/Resources/public/js/pwa-i18n-de.js
+++ src/Resources/public/js/pwa-i18n-de.js
@@ -4,7 +4,7 @@
  * @version 8
  * @author con4gis contributors (see "authors.txt")
  * @license LGPL-3.0-or-later
- * @copyright (c) 2010-2021, by Küstenschmiede GmbH Software & Design
+ * @copyright (c) 2010-2022, by Küstenschmiede GmbH Software & Design
  * @link https://www.con4gis.org
  */
 
