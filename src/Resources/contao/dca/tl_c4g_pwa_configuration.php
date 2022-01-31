70494bb3aaed430064a000db82077bdd689bfb9e
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-01-31 09:31:28 +0100

    Version preps

--- src/Resources/contao/dca/tl_c4g_pwa_configuration.php
+++ src/Resources/contao/dca/tl_c4g_pwa_configuration.php
@@ -85,7 +85,7 @@ $GLOBALS['TL_DCA'][$strName] = array
                 'label'         => &$GLOBALS['TL_LANG'][$strName]['delete'],
                 'href'          => 'act=delete',
                 'icon'          => 'delete.svg',
-                'attributes'    => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false;Backend.getScrollOffset()"',
+                'attributes'    => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\')) return false;Backend.getScrollOffset()"',
             ),
             'show' => array
             (
